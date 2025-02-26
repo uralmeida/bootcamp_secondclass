c0ae6c2d808448771c0fc35ea02b4f8d76a60878
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-26 16:45:50 -0300

    adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -43,6 +43,11 @@ import math
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
+raio_do_circulo = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio_do_circulo **2
+
+print(f"{area_do_circulo:.2f}")
+
 #raio_do_circulo = float(input("Digite o raio: "))
 #area_do_circulo = math.pi * raio_do_circulo ** 2
 # area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
